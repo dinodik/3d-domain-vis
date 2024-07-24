f56d7ad7e65a4d287bf6e2b536d31a2341bccfb6
Louis Sun <xw.louissun@gmail.com>
2024-07-24 20:38:12 +0800

    rewritten to use three.js, simple plotting works

--- src/main.ts
+++ src/main.ts
@@ -1,101 +1,105 @@
-import { initBuffers } from "./init-buffers"
-import { drawScene } from "./draw-scene"
+import * as THREE from 'three';
 
-main();
+let [width, height] = [400, 300];
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
 
-function loadShader(gl: WebGLRenderingContext, type: GLenum, source: string): WebGLShader {
-    const shader = gl.createShader(type);
-    if (!shader)
-        throw new Error(`Could not create WebGL shader of type ${type}`);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        throw new Error(`An error occured compiling the shader: ${gl.getShaderInfoLog(shader)}`)
-        // gl.deleteShader(shader);
-    }
-    return shader;
+function f(x: number, z: number, target: THREE.Vector3): void {
+    const y =  Math.cos(Math.sqrt(x**2 + z**2));
+    target.set(x, y, z)
 }
 
-function initShaderProgram(gl: WebGLRenderingContext, vsSource: string, fsSource: string): WebGLProgram {
-    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+const gridSize = 20;
+const numPoints = gridSize + 1;
+const [min, max] = [-7, 7];
+const dx = (max - min) / gridSize;
 
-    const shaderProgram = gl.createProgram();
-    if (!shaderProgram)
-        throw new Error("Failed to create new shader program");
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
+const EPS = 0.0001;
+const p0 = new THREE.Vector3(), p1 = new THREE.Vector3();
+const px = new THREE.Vector3(), pz = new THREE.Vector3();
+const pn = new THREE.Vector3()
+const vertices = new Float32Array(3 * numPoints**2);
+const normals = new Float32Array(3 * numPoints**2);
+for (let j = 0; j < numPoints; ++j) {
+    const z = min + j * dx;
+    for (let i = 0; i < numPoints; ++i) {
+        const idx = 3 * (j*numPoints + i);
+        const x = min + i * dx;
+        
+        f(x, z, p0);
 
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        throw new Error(`Unable to link the shader program: ${gl.getProgramInfoLog(shaderProgram)}`);
+        vertices[idx] = p0.x;
+        vertices[idx+1] = p0.y;
+        vertices[idx+2] = p0.z;
+
+        f(x+EPS, z, p1);
+        px.subVectors(p1, p0);
+        f(x, z+EPS, p1);
+        pz.subVectors(p1, p0);
+        pn.crossVectors(pz, px).normalize();
+
+        normals[idx] = pn.x;
+        normals[idx+1] = pn.y;
+        normals[idx+2] = pn.z;
     }
-    return shaderProgram;
+}
+console.log(vertices);
+console.log(normals);
+
+const indices: number[] = Array(6 * gridSize**2);
+for (let j = 0; j < gridSize; ++j) {
+    for (let i = 0; i < gridSize; ++i) {
+        const idx = 6 * (j * gridSize + i);
+        const a = j * numPoints + i;
+        const b = j * numPoints + i + 1;
+        const c = (j+1) * numPoints + i;
+        const d = (j+1) * numPoints + i + 1;
+        // triangle 1
+        indices[idx+0] = a;
+        indices[idx+1] = c;
+        indices[idx+2] = b;
+        // triangle 2
+        indices[idx+3] = b;
+        indices[idx+4] = c;
+        indices[idx+5] = d;
+    }
 }
 
-function main(): void {
-    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
-    const gl = canvas.getContext("webgl");
+const geometry = new THREE.BufferGeometry();
+geometry.setIndex(indices);
+geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+geometry.setAttribute("normal", new THREE.BufferAttribute(vertices, 3));
 
-    if (gl == null) {
-        throw new Error("Unable to initialise WebGL");
-    }
+const material = new THREE.MeshNormalMaterial();
+// const material = new THREE.MeshBasicMaterial({
+//     color: 0xff0000,
+//     wireframe: true,
+// });
+// const material = new THREE.MeshPhongMaterial()
+material.side = THREE.DoubleSide;
+// material.shininess = 100;
+// material.specular = new THREE.Color(0x1188ff);
 
-    const vsSource = `
-        attribute vec4 aVertexPosition;
-        attribute vec3 aVertexNormal;
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-        uniform mat4 uModelViewMatrix;
-        uniform mat4 uProjectionMatrix;
-        uniform mat4 uNormalMatrix;
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
-        varying lowp vec4 vColour;
+const pointLight = new THREE.PointLight(0xffffff, 1.7);
+pointLight.position.x = 0;
+pointLight.position.y = 5;
+pointLight.position.z = 0;
+scene.add(pointLight);
 
-        void main() {
-            gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+camera.position.y = 3;
+camera.position.z = 15;
 
-            highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
-            vColour = transformedNormal;
-        }
-    `;
-
-    const fsSource = `
-        varying lowp vec4 vColour;
-        void main() {
-            gl_FragColor = vColour;
-        }
-    `;
-
-    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
-    const programInfo = {
-        program: shaderProgram,
-        attribLocations: {
-            vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
-            // vertexColour: gl.getAttribLocation(shaderProgram, "aVertexColour"),
-            vertexNormal: gl.getAttribLocation(shaderProgram, "aVertexNormal"),
-        },
-        uniformLocations: {
-            projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
-            modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
-            normalMatrix: gl.getUniformLocation(shaderProgram, "uNormalMatrix"),
-        },
-    };
-
-    const buffers = initBuffers(gl);
-
-    let squareRotation = 0;
-    let deltaTime = 0;
-    let then = 0;
-    function render(now: number) {
-        now *= 0.001; // ms to s
-        deltaTime = now - then;
-        then = now;
-
-        drawScene(gl as WebGLRenderingContext, programInfo, buffers, squareRotation);
-        squareRotation += deltaTime;
-        requestAnimationFrame(render);
-    }
-    requestAnimationFrame(render);
+function animate() {
+    // mesh.rotateX(0.01);
+    renderer.render(scene, camera);
 }
+renderer.setAnimationLoop(animate);
