1e9f0e052d6b34f95dc9e20cd425429a38c83577
Louis Sun <xw.louissun@gmail.com>
2024-07-27 23:16:12 +0800

    refactored structure and all surfaces can be plotted

---
+++ src/Axis.ts
@@ -0,0 +1,187 @@
+import * as Utils from "./Utils";
+import { Mesh } from "three";
+
+interface GroupVertexInfo {
+    top: Utils.VertexInfo,
+    bot: Utils.VertexInfo,
+    back: Utils.VertexInfo,
+    front: Utils.VertexInfo,
+    left: Utils.VertexInfo,
+    right: Utils.VertexInfo,
+};
+
+interface GroupMeshConfig {
+    top: MeshConfig,
+    bot: MeshConfig,
+    back: MeshConfig,
+    front: MeshConfig,
+    left: MeshConfig,
+    right: MeshConfig,
+};
+
+interface MeshConfig {
+    hidden: boolean,
+    transparent: boolean,
+    needsUpdate: boolean,
+}
+
+export class Visualiser {
+    minX: number = -5;
+    maxX: number = 5;
+    minY: number = -5;
+    maxY: number = 5;
+    minZ: number = -5;
+    maxZ: number = 5;
+
+    density: number = 2;
+
+    discreteDomain: Utils.GridXZ;
+    discreteRegion: Utils.GridXZ;
+
+    bounds: Utils.BoundsXZ;
+
+    vertexInfos: {
+        bounding: GroupVertexInfo,
+        region: GroupVertexInfo,
+    };
+
+    meshConfigs: {
+        bounding: GroupMeshConfig,
+        region: GroupMeshConfig,
+    };
+
+    readonly faceNames = ["top", "bot", "back", "front", "left", "right"] as const;
+
+    top: ((x: number, z: number) => number) = ((x, z) => Math.cos(Math.sqrt(x**2 + z**2)) + 2); // 2*x + 3*z);
+    bot: ((x: number, z: number) => number) = ((x, z) => -2); // x + z);
+
+    constructor(bounds: Utils.BoundsXZ) {
+        this.bounds = bounds;
+        this.discreteDomain = this._discretiseDomain();
+        this.discreteRegion = Utils.discretiseArea(this.bounds, this.density);
+
+        this.vertexInfos = {
+            bounding: this._generateBoundingMeshes(),
+            region: this._generateRegionMeshes(),
+        }
+
+        this.meshConfigs = this._initMeshConfig();
+    }
+
+    // this is so awful
+    _initMeshConfig(): {bounding: GroupMeshConfig, region: GroupMeshConfig} {
+        const config: any = { bounding: {}, region: {} };
+        Object.keys(config).forEach((key: string) => {
+            const group: any = {};
+            this.faceNames.forEach((face) => {
+                group[face] = {
+                    hidden: false,
+                    transparent: false,
+                    needsUpdate: false,
+                } as MeshConfig;
+            });
+            config[key] = group as GroupMeshConfig;
+        });
+        return config;
+    }
+
+    _discretiseDomain(): Utils.GridXZ {
+        const bounds: Utils.BoundsXZ = {
+            back: (() => this.minZ),
+            front: (() => this.maxZ),
+            left: this.minX,
+            right: this.maxX,
+        }
+        return Utils.discretiseArea(bounds, this.density);
+    }
+
+    _generateBoundingMeshes(): GroupVertexInfo {
+        const topInfo = Utils.generateSurface(this.top, this.discreteDomain);
+        const botInfo = Utils.generateSurface(this.bot, this.discreteDomain);
+
+        // back/front
+        const X = this.discreteDomain.x
+        const minY = () => this.minY;
+        const maxY = () => this.maxY;
+        const backInfo = Utils.wallAlongZ(X, this.bounds.back, minY, maxY)
+        const frontInfo = Utils.wallAlongZ(X, this.bounds.front, minY, maxY);
+
+        // left/right
+        const [x1, x2, z1, z2, y1, y2] = [this.bounds.left, this.bounds.right, this.minZ, this.maxZ, this.minY, this.maxY];
+        const leftInfo = Utils.knitLines(
+            new Float32Array([x1, y2, z1, x1, y2, z2]),
+            new Float32Array([x1, y1, z1, x1, y1, z2]),
+            2,
+            new Float32Array([-1, 0, 0, -1, 0, 0]),
+        )
+        const rightInfo = Utils.knitLines(
+            new Float32Array([x2, y2, z1, x2, y2, z2]),
+            new Float32Array([x2, y1, z1, x2, y1, z2]),
+            2,
+            new Float32Array([1, 0, 0, 1, 0, 0]),
+        )
+
+        return {
+            top: topInfo,
+            bot: botInfo,
+            back: backInfo,
+            front: frontInfo,
+            left: leftInfo,
+            right: rightInfo,
+        };
+    }
+
+    _generateRegionMeshes(): GroupVertexInfo {
+        const topInfo = Utils.generateSurface(this.top, this.discreteRegion, true);
+        const botInfo = Utils.generateSurface(this.bot, this.discreteRegion, false);
+
+        // back/front - harder to extract from top/bot info so just recalculate
+        const X = this.discreteRegion.x
+        const minY = this.bot;
+        const maxY = this.top;
+        const backInfo = Utils.wallAlongZ(X, this.bounds.back, minY, maxY)
+        const frontInfo = Utils.wallAlongZ(X, this.bounds.front, minY, maxY);
+        
+        // left
+        let leftInfo: Utils.VertexInfo;
+        {
+            const num = this.discreteRegion.numsZ[0];
+            const topLeft = topInfo.vertices.slice(0, 3*num);
+            const botLeft = botInfo.vertices.slice(0, 3*num);
+            const normals = new Float32Array([].concat(...Array(num).fill([-1, 0, 0])));
+            leftInfo = Utils.knitLines(topLeft, botLeft, num, normals);
+        }
+
+        // right
+        let rightInfo: Utils.VertexInfo;
+        {
+            const num = this.discreteRegion.numsZ.at(-1);
+            if (!num)
+                throw new Error("The mesh has zero depth, there should at least be one vertex");
+            const topRight = topInfo.vertices.slice(-3*num);
+            const botRight = botInfo.vertices.slice(-3*num);
+            const normals = new Float32Array([].concat(...Array(num).fill([-1, 0, 0])));
+            rightInfo = Utils.knitLines(topRight, botRight, num, normals);
+        }
+
+        return {
+            top: topInfo,
+            bot: botInfo,
+            back: backInfo,
+            front: frontInfo,
+            left: leftInfo,
+            right: rightInfo,
+        };
+    }
+
+
+    update(): void {
+        
+    }
+
+    getMeshes(): Mesh[] {
+        // return [Utils.meshFromInfo(this.meshInfos.region.left), Utils.meshFromInfo(this.meshInfos.region.top)];
+        const allInfos = Object.values(this.vertexInfos.bounding).concat(Object.values(this.vertexInfos.region));
+        return allInfos.map(info => Utils.meshFromInfo(info));
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
 import * as THREE from 'three';
+import { Visualiser } from './Axis';
+import { BoundsXZ } from './Utils';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-
-// calcuate domain values first then just pass into functions
-
-let [width, height] = [400, 300];
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer();
-const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-const controls = new OrbitControls(camera, renderer.domElement);
-renderer.setSize(width*2, height*2);
-document.body.appendChild(renderer.domElement);
 
 function f(x: number, z: number): number {
     return Math.cos(Math.sqrt(x**2 + z**2)) + 2;
@@ -27,223 +19,41 @@ function g2(x: number): number {
     return Math.sin(x) + 4;
 }
 
-const EPS = 0.0001;
-const density = 1;
-
-interface VertexInfo {
-    vertices: number[],
-    indices: number[],
-    normals: number[],
-    width?: number,
-    depths?: number[],
+const bounds: BoundsXZ = {
+    back: g1,
+    front: g2,
+    left: -3,
+    right: 5,
 }
 
-// (x, y, z) s.t. {y = func(x, z), g1(x) < z < g2(x), a < x < b}
-// TODO handling of mismatched columns is botched
-function generateSurface3D(
-    func: ((x: number, z: number) => number),
-    g1: ((x: number) => number), g2: ((x: number) => number),
-    a: number, b: number,
-    upIsOut?: true,
-): VertexInfo {
-    // TODO: make these arrays?
-    const vertices: number[] = [];
-    const indices: number[] = [];
-    const normals: number[] = [];
-    const depths: number[] = [];
+const vis = new Visualiser(bounds);
 
-    const numX = Math.ceil((b - a) * density) + 1;
-    const dx = (b - a) / (numX - 1);
+let [width, height] = [400, 300];
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer();
 
-    // Useful vectors to keep around
-    const p0 = new THREE.Vector3
-    const px = new THREE.Vector3(EPS, 0, 0), pz = new THREE.Vector3(0, 0, EPS);
-    const pn = new THREE.Vector3;
-    
-    const G1 = new Float32Array(numX);
-    const G2 = new Float32Array(numX);
+// const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+const camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 );
+camera.zoom = 0.8 * Math.min(width / (vis.maxX - vis.minX), height / (vis.maxY - vis.minY));
+camera.updateProjectionMatrix();
+camera.updateMatrix();
 
-    let lastNumZ = 0;
-    let idx = 0;
-    for (let i = 0; i < numX; ++i) {
-        depths.push(idx); // the index of the first element of every column
+scene.add( camera );
+const controls = new OrbitControls(camera, renderer.domElement);
+renderer.setSize(width, height);
+scene.frustumCulled = false;
+document.body.appendChild(renderer.domElement);
 
-        const x = a + i*dx
-        const [c, d] = [g1(x), g2(x)];
-        G1[i] = c;
-        G2[i] = d;
-        // TODO: alternatively sample at regular grid points then add an additional row
-        const numZ = Math.ceil((d - c) * density) + 1;
-        const diff = lastNumZ - numZ; // for seeing if columns are mismatched
-        const dz = (d - c) / (numZ - 1); // check if inf
-        
-        p0.x = x;
-        for (let k = 0; k < numZ; ++k) {
-            const z = c + k*dz;
-            // Get vertex values
-            p0.z = z;
-            p0.y = func(x, z);
-            vertices.push(p0.x, p0.y, p0.z);
+vis.getMeshes().map((mesh) => scene.add(mesh));
 
-            // Get normal values
-            px.y = func(x + EPS, z) - p0.y;
-            pz.y = func(x, z + EPS) - p0.y;
-            pn.crossVectors(pz, px).normalize(); // check upIsOut
-            normals.push(pn.x, pn.y, pn.z);
+vis.getMeshes().forEach((info) => {
+    scene.add(info);
+});
 
-            // Get indices (+x right, +z down);
-            if (i !== 0 && k !== 0) {
-                if (k < lastNumZ) {
-                    const [botRight, topRight, botLeft, topLeft] = [idx, idx-1, idx-lastNumZ, idx-lastNumZ-1];
-                    indices.push(topLeft, botLeft, topRight);
-                    indices.push(topRight, botLeft, botRight);
-                    if (k === numZ-1) { // check if extra triangles are needed in case vertex shortage
-                        for (let j = 0; j < diff; ++j) {
-                            indices.push(idx, idx-lastNumZ+j, idx-lastNumZ+j+1);
-                        }
-                    }
-                } else { // there is an excess of verts
-                    indices.push(idx, idx-1, idx-k-1);
-                }
-            }
-            
-            ++idx;
-        }
-
-        lastNumZ = numZ;
-    }
-    
-    return {
-        vertices: vertices,
-        normals: normals,
-        indices: indices,
-        width: numX,
-        depths: depths,
-    };
-}
-
-// knits together two surfaces that have the same x-z shadow
-function joinSurfaces3D(top: VertexInfo, bot: VertexInfo): VertexInfo {
-    if (top.vertices.length !== bot.vertices.length)
-        throw new Error("Surface mismatch!");
-
-    if (!top.width || !top.depths)
-        throw new Error("Missing width and depth info for surfaces");
-
-    const numPoints = top.vertices.length;
-    const width = top.width;
-    const depths = top.depths;
-        
-    const vertices: number[] = [];
-    const normals: number[] = [];
-    // const indices = top.indices.concat(bot.indices);
-    const indices: number[] = [];
-
-    // left
-    vertices.push(...top.vertices.slice(0, 3*depths[1]));
-    vertices.push(...bot.vertices.slice(0, 3*depths[1]));
-    for (let i = 0; i < depths[1]-1; ++i) {
-        indices.push(i, i+depths[1], i+1);
-        indices.push(i+1, i+depths[1], i+depths[1]+1);
-    }
-
-    return {
-        vertices: vertices,
-        normals: normals,
-        indices: indices,
-    };
-}
-
-const zUp = new THREE.Matrix4(
-    0, 1, 0, 0,
-    0, 0, 1, 0,
-    1, 0, 0, 0,
-    0, 0, 0, 1,
-);
-
-function meshFromVertexInfo(info: VertexInfo): THREE.Mesh {
-    const geometry = new THREE.BufferGeometry();
-    geometry.setIndex(info.indices);
-    geometry.setAttribute("position", new THREE.BufferAttribute(new Float32Array(info.vertices), 3));
-    geometry.setAttribute("normal", new THREE.BufferAttribute(new Float32Array(info.normals), 3));
-    // const material = new THREE.MeshBasicMaterial({wireframe: true});
-    // const material = new THREE.MeshPhongMaterial({color: 0x550000, transparent: false});
-    const material = new THREE.MeshNormalMaterial();
-    material.side = THREE.DoubleSide;
-    const mesh = new THREE.Mesh(geometry, material);
-    // mesh.applyMatrix4(zUp);
-    return mesh;
-}
-
-// const [w, d] = [infoTop.width, infoTop.depth];
-// if (!w || !d)
-//     throw new Error("need width and depth!");
-
-// for (let i = 0; i < w; ++i) {
-//     const idxBack = 3*i;
-//     const idxFront = 3*w*(d-1) + 3*i;
-//     vertices.push(...infoTop.vertices.slice(idxBack, idxBack+3));
-//     vertices.push(...infoBot.vertices.slice(idxBack, idxBack+3));
-//     vertices.push(...infoTop.vertices.slice(idxFront, idxFront+3));
-//     vertices.push(...infoBot.vertices.slice(idxFront, idxFront+3));
-//     if (i < w - 1) {
-//         const idx = 4*i;
-//         {
-//             const [a, b, c, d] = [idx, idx+1, idx+4, idx+4+1];
-//             indices.push(a, c, b);
-//             indices.push(b, c, d);
-//         }
-//         {
-//             const [a, b, c, d] = [idx+2, idx+3, idx+4+2, idx+4+3];
-//             indices.push(a, b, c);
-//             indices.push(b, d, c);
-//         }
-//     }
-//     normals.push(0, 0, -1);
-//     normals.push(0, 0, -1);
-//     normals.push(0, 0, 1);
-//     normals.push(0, 0, 1);
-// }
-
-// const skip = vertices.length/3;
-// for (let i = 0; i < d; ++i) {
-//     const idxLeft = 3*w*i;
-//     const idxRight = 3*w*i + 3*(w-1);
-//     vertices.push(...infoTop.vertices.slice(idxLeft, idxLeft+3));
-//     vertices.push(...infoBot.vertices.slice(idxLeft, idxLeft+3));
-//     vertices.push(...infoTop.vertices.slice(idxRight, idxRight+3));
-//     vertices.push(...infoBot.vertices.slice(idxRight, idxRight+3));
-//     if (i < d - 1) {
-//         const idx = skip+4*i;
-//         {
-//             const [a, b, c, d] = [idx, idx+1, idx+4, idx+4+1];
-//             indices.push(a, b, c);
-//             indices.push(b, d, c);
-//         }
-//         {
-//             const [a, b, c, d] = [idx+2, idx+3, idx+4+2, idx+4+3];
-//             indices.push(a, c, b);
-//             indices.push(b, c, d);
-//         }
-//     }
-//     normals.push(-1, 0, 0);
-//     normals.push(-1, 0, 0);
-//     normals.push(1, 0, 0);
-//     normals.push(1, 0, 0);
-// }
-
-// const infoWall: VertexInfo = {
-//     vertices: vertices,
-//     indices: indices, 
-//     normals: normals,
-// }
-
-const topInfo = generateSurface3D(f, g1, g2, -5, 5);
-const botInfo = generateSurface3D(g, g1, g2, -5, 5);
-// const volumeInfo = joinSurfaces3D(topInfo, botInfo);
-// scene.add(meshFromVertexInfo(volumeInfo));
-scene.add(meshFromVertexInfo(topInfo));
-scene.add(meshFromVertexInfo(botInfo));
+// const cube = new THREE.BoxGeometry(1, 1, 1);
+// const mat = new THREE.MeshBasicMaterial({color: 0xff0000})
+// const mesh = new THREE.Mesh(cube, mat);
+// scene.add(mesh);
 
 // LIGHTING
 // const ambientLight = new THREE.AmbientLight(0xffffff, 5);
